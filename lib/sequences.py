5e7defdf06b13b23e44f7f0674df34c3596cf669
tray <onesimus.githua@student.moringaschool.com>
2023-11-23 17:24:56 +0300

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print('[]')
+        return
+    elif length == 1:
+        print('[0]')
+        return
+
+    fibonacci_sequence = [0, 1]
+    while len(fibonacci_sequence) < length:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence)
